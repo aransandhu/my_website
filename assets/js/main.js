a1eb941df96858cf51ef0232453a5ec84ad44aa1
Aran Sandhu <aran@Arans-MacBook-Pro.local>
2018-09-15 20:40:39 -0400

    Files

---
+++ assets/js/main.js
@@ -0,0 +1,86 @@
+/*
+	Photon by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	// grab an element
+	var myElement = document.querySelector("header");
+// construct an instance of Headroom, passing the element
+	// var headroom  = new Headroom(myElement);
+
+
+var headroom = new Headroom(myElement, {
+  "offset": 0,
+  "tolerance": 0,
+  "classes": {
+    // "initial": "headroom",
+    "pinned": "slideDown",
+    "unpinned": "slideUp",
+    "top": "headroom--top"
+  },
+
+
+});
+
+
+// initialise
+	headroom.init(); 
+
+
+
+	skel.breakpoints({
+		xlarge: '(max-width: 1680px)',
+		large: '(max-width: 1140px)',
+		medium: '(max-width: 980px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)',
+		xxsmall: '(max-width: 320px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 250);
+			});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on mobile.
+			skel.on('+mobile -mobile', function() {
+				$.prioritize(
+					'.important\\28 mobile\\29',
+					skel.breakpoint('mobile').active
+				);
+			});
+
+		// Scrolly.
+			$('.scrolly').scrolly();
+
+	});
+
+
+	$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 1000, 'linear');
+  });
+});
+
+
+
+})(jQuery);
+
+
+
